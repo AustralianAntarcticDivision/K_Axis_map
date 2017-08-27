c8c77aa8641c7ea0abfe4620bcbf827ced5373b1
RTreb <rowan.trebilco@utas.edu.au>
2017-08-28 09:44:36 +1000

    add extra locations example

--- k_axis_map_script_1.R
+++ k_axis_map_script_1.R
@@ -20,7 +20,7 @@ prj<- "+proj=laea +lat_0=-60 +lon_0=75 +datum=WGS84 +ellps=WGS84 +no_defs +towgs
 
 
 # plot extent
-ras.ext   <- raster(xmn=60, xmx=95, ymn=-70, ymx=-40) # extent for zoomed map
+ras.ext   <- raster(xmn=60, xmx=105, ymn=-70, ymx=-40) # extent for zoomed map
 ras.ext2  <- raster(xmn=-180, xmx=180, ymn=-90, ymx=0) # full SO extent to make fill play nicely
 
 # cropped and full versions of world map
@@ -76,13 +76,38 @@ ofp<- spTransform(orsifronts, CRS(prj))
 # grat lines
 xx <- c(0,30, 60, 90, 120,150,180); yy <- c(-90,-80, -70,-60, -50,-40,-30,-20)
 
+# if there are locations you want to show
+# (note that the structure() call below just does the same thing as reading a table of locations from text/csv)
+locs <- structure(list(Cruise = structure(c(3L, 3L, 3L, 3L, 3L, 3L, 3L, 
+2L), .Label = c("", "V2", "V3"), class = "factor"), Station = structure(c(7L, 
+3L, 4L, 9L, 6L, 2L, 8L, 5L), .Label = c("", "R23", "R29", "R37", 
+"R8", "T11", "T20", "T29", "T40"), class = "factor"), Type = structure(c(3L, 
+2L, 2L, 3L, 3L, 2L, 3L, 2L), .Label = c("", "R", "T"), class = "factor"), 
+    Date.Time = structure(c(2L, 2L, 2L, 2L, 2L, 2L, 2L, 3L), .Label = c("", 
+    "2016-01-01T00:00", "2016-12-21T00:00"), class = "factor"), 
+    Long = c(81.886667, 83.353333, 83.580267, 63.9278, 83.8392, 
+    85.033367, 77.616784, 110.0646846), Lat = c(-59.921667, -57.93, 
+    -60.319233, -66.17905, -64.266017, -61.933375, -61.283533, 
+    -66.1313351), Depth = c(220L, 20L, 10L, 20L, 15L, 10L, 60L, 
+    160L), Species = structure(c(3L, 4L, 4L, 2L, 2L, 3L, 2L, 
+    5L), .Label = c("", "E. superba", "E. triacantha", "E. vallentini", 
+    "E.crystallorophias"), class = "factor")), .Names = c("Cruise", 
+"Station", "Type", "Date.Time", "Lon", "Lat", "Depth", "Species"
+), row.names = c(NA, 8L), class = "data.frame")
+# convert to spatial points df
+coordinates(locs) <- c("Lon", "Lat")
+# define projection as longlat
+projection(locs) <- "+proj=longlat +datum=WGS84"
+# reproject to map
+locs <- spTransform(locs, CRS(prj))
+
 # TODO:
 # orsi fronts
 # max/min ice extent
 # subset to midoc (/RMT/CTD) stations
 # add labelling
 
-pdf("K_axis_stations_map.pdf", width=4, height=4)
+pdf("K_axis_stations_map.pdf", width=4.5, height=4)
 op<- par(mar=rep(0,4), oma=rep(0.5,4))
 plot(wcp, border=NA)
 
@@ -96,11 +121,19 @@ plot(rasterToContour(pin, lev = 15),add = TRUE, lty=1, col="#6BAED6", lwd=1.5)
 plot(wp, add=T, col="darkgrey", border=F)
 plot(graticule(lons = xx, lats = yy,  proj = prj), add=T, lty=2, col="gray40")
 lines(ks$longitude, ks$latidue)
-points(ks, pch=19, col="gray20")
+# can add large points for midoc stations by un-commenting line below
+#points(ks, pch=19, col="gray20")
 g1labs <- graticule_labels(lons=c(150, 120,90,60), xline=180, yline=-50, proj=projection(prj))
 text(coordinates(g1labs[g1labs$islon, ]), lab=parse(text=g1labs$lab[g1labs$islon]), pos=3, cex=0.8, col="gray30")
 g2labs <- graticule_labels(lats=c(-40, -50,-60,-70), , xline=60, yline=-50, proj=projection(prj))
 text(coordinates(g2labs[!g2labs$islon, ]), lab=parse(text=g2labs$lab[!g2labs$islon]), pos=1, cex=0.8,col="gray30")
+
+# to add extra points for other locations (locs dataframe)
+points(locs, col="blue", pch = 19)
+# or subsets
+points(locs[locs$Cruise=="V2",], pch = 19, col="red")
+# if you want labels
+#text(locs, labels=locs$Species, pos=2, font=3)
 
 # can add labels... position will need teaking
 #text(60,-61.75,"SACCF",cex=1,adj=0,srt=-20)
